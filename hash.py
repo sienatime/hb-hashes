5b844dec91b36165d2e8744be53949a00c6af0d3
Siena Aguayo <siena.aguayo@gmail.com>
2013-09-30 22:37:52 -0700

    consulted with the bf and fixed things up

--- hash.py
+++ hash.py
@@ -4,48 +4,43 @@
 # get(h, key)
 #     return value
 
-# where h is a list
-
-# start with a hash table size of 100 (so mod hash value by 100 to get bucket)
-
-# python has a hash() function
-
-keys_values = []
+table_size = 100
 
 def hash_insert(h, key, value):
+    hash_index = hash(key) % table_size
 
-    keys_values.append( (key,value) )
+    list_of_values = h[hash_index]
 
-    hash_index = hash(key) % 1
-
-    if h[hash_index] == []:
-        h[hash_index] = [value]
-    else: 
-        h[hash_index].append(value)
+    #if key is already there, replace it with new value
+    for tuples in list_of_values:
+        if key == tuples[0]:
+            list_of_values.remove(tuples)
+    
+    list_of_values.append( (key, value) )
 
 def hash_get(h, key):
-    hash_index = hash(key) % 1
+    hash_index = hash(key) % table_size
 
-    thelist = h[hash_index]
-    return thelist
-    #return keys_values
-    # how do we get the right value using keys_values???
+    for tuples in h[hash_index]:
+        if key == tuples[0]:
+            return tuples[1]
 
 def main():
     # do stuff
 
-    hash_table = [[]] * 100
+    hash_table = []
+
+    # NOTE: hash_table = [[]] * table_size will make each empty list point to the same object so don't do it.
+    for i in range(table_size):
+        hash_table.append([])
 
     hash_insert(hash_table, 1, "one")
-
-    #keep list of tuples that is key, value
-
-    print hash_table
-
     hash_insert(hash_table, 10, "ten")
-
-    print hash_table
-
-    print hash_get(hash_table, 1)
+    hash_insert(hash_table, "some key","some value")
+    #print "hash_table:", hash_table
+    print "get key 10:", hash_get(hash_table, 10)
+    print "get some key:", hash_get(hash_table, "some key")
+    hash_insert(hash_table, "some key","another value")
+    print "get some key:", hash_get(hash_table, "some key") 
 
 main()
